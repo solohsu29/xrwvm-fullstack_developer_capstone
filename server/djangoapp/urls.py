d6cad7f756eb289cabff93f5b52fb783b9c7a81c
solohsu29 <solohsu321@gmail.com>
2024-12-13 10:28:57 -0500

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,25 +1,31 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
-
 urlpatterns = [
-    # Path for registration
-    path('registration/', views.registration, name='registration'),
-    # Path for login
-    path('login/', views.login_user, name='login'),
-    # Path for logout
-    path('logout/', views.logout_request, name='logout'),
-    # Path for retrieving car models
-        path(route='get_cars', view=views.get_cars, name ='getcars'),
-    # Path for retrieving dealerships
+    # # path for registration
+    path(route='register', view=views.registration, name='register'),
+
+    # path for login
+    path(route='login', view=views.login_user, name='login'),
+
+    # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
+
+    # path for cars
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
+
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='get_dealer_details'),
+
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     
-    # Path for retrieving dealership details
-    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
-    # Path for retrieving dealer reviews
-        path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-    # Path for adding a review
-        path(route='add_review', view=views.add_review, name='add_review'),
-] 
+    path(route='add_review', view=views.add_review, name='add_review'),
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
